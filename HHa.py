089736ac8763518101779adf20df1ed6e83a791d
youssef <71567682+frudrik@users.noreply.github.com>
2021-07-19 20:56:33 +0100

    Add files via upload

---
+++ HHa.py
@@ -0,0 +1,151 @@
+
+import time,os,sys
+def jalan(z):
+    for e in z+"\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
+
+
+jalan("\033[1;34m╔═══╗───╔╗─╔════╗")
+jalan("\033[1;34m║╔══╝───║║─║╔╗╔╗║")
+jalan("\033[1;34m║╚══╦╗╔╦╣║─╚╝║║╠╣╔╗╔╦╦═╗╔═══╗")
+jalan("\033[1;34m║╔══╣╚╝╠╣║───║║║╚╝╚╝╠╣╔╗╬══║║")
+jalan("\033[1;34m║╚══╬╗╔╣║╚╗──║║╚╗╔╗╔╣║║║║║══╣")
+jalan("\033[1;34m╚═══╝╚╝╚╩═╝──╚╝─╚╝╚╝╚╩╝╚╩═══╝")
+jalan("\033[1;33m############## youssef haqi #############")
+jalan("@@@@@@@@@@@@ channel telegram https://t.me/Eviltwinz")
+print("                          my watsapp: 0659123803")
+print("                      my facebook: https://www.facebook.com/youssef.haqi")
+def list( ):
+    print("[1] Update termux")
+print("Hacker facebook")
+jalan("\033[1;31m[3] install xNot_Found")
+jalan("\033[1;31m[4] install facebash")
+jalan("\033[1;31m[5] install faceboom")
+jalan("\033[1;31m[6] install FB-Cracker")
+jalan("\033[1;31m[7] install fbhack")
+print("tools root")
+jalan("\033[1;34m[8] install kalinethunter")
+jalan("\033[1;34m[9] install kali linux ")
+jalan("\033[1;34m[10] install proot")
+jalan("\033[1;34m[11] install ubuntu")
+jalan("\033[1;34m[12] install kalimux ")
+print("tools fishing")
+jalan("\033[1;32m[13] install SocialFish")
+jalan("\033[1;32m[14] install SocialSploit")
+jalan("\033[1;32m[15] install shellphish")
+jalan("\033[1;32m[16] install weeman")
+jalan("\033[1;32m[17] install blackeye")
+print("")
+print("")
+list()
+choose = input("[?] choose : ")
+
+if choose =="1":
+        os.system("pkg update && upgrade -y")
+        jalan("update")
+        time.sleep(2)
+        os.system("python2 HHa.py")
+
+
+elif choose =="2":
+        os.system("pkg update && upgrade ")
+        jalan("all pkg have been installed")
+        time.sleep(2)
+        os.system("python2 HHa.py")
+
+elif choose =="3":
+        os.system("git clone https://github.com/hatakecnk/xNot_Found.git")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHai.py")
+
+elif choose =="4":
+        os.system("git clone https://github.com/thelinuxchoice/facebas")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="5":
+        os.system("https://github.com/facebook/facebook-python-business-sdk")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HH.py")
+
+elif choose =="6":
+        os.system("git clone https://github.com/pashayogi/SETAN")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+
+elif choose =="7":
+        os.system(" git clone https://github.com/gutclone/fbhack")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="8":
+        os.system("curl -LO https://raw.githubusercontent.com/Hax4us/Nethunter-In-Termux/master/kalinethunter")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="9":
+        os.system("wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/SSH/Apt/ssh-apt.sh && bash ssh-apt.sh")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="10":
+        os.system("wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="11":
+        os.system("git clone https://github.com/MFDGaming/ubuntu-i...​")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="12":
+        os.system("git clone https://github.com/404system/kalimuxTools/")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="13":
+        os.system("git clone https://github.com/Lexiie/SocialFish.git")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="14":
+        os.system("git clone httgs://github.com/Cesar-hack-Gray/SocialSploit")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="15":
+        os.system("git clone https://github.com/thelinuxchoice/shellphish")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="16":
+        os.system("git clone https://github.com/evait-security/weeman")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+elif choose =="17":
+        os.system("git clone https://github.com/thelinuxchoice/blackeye")
+        jalan("all tools have been installed")
+        time.sleep(2)
+        os.system("python3 HHa.py")
+
+else:
+    jalan("\033[1;36mSorry, wrong choice. Choose from number 1 to 17")
